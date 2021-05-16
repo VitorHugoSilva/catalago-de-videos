74da652244846f56f2d21da8c303c0bae344f879
Vitor Hugo <vhsilva.ap@gmail.com>
2021-05-15 21:43:22 -0300

    Teste para deploy

--- .docker/app/entrypoint.sh
+++ .docker/app/entrypoint.sh
@@ -4,8 +4,10 @@ cp .env.testing.example .env.testing
 composer install
 php artisan key:generate
 php artisan key:generate --env=testing
-php artisan migrate --seed
 
 chmod -R 777 .
+
+php artisan migrate --seed
+
 
 php-fpm
